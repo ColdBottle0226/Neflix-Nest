7df9b052adf2d02f8ab18b3a9386d0430528a878
bottleCold <kbc3978@m2mglobal.co.kr>
2026-03-10 23:33:56 +0900

    업데이트 DTO 적용

--- src/movie/dto/update-movie.dto.ts
+++ src/movie/dto/update-movie.dto.ts
@@ -1,6 +1,17 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateMovieDto } from './create-movie.dto';
-import { IsNotEmpty, IsOptional } from 'class-validator';
+import { Contains, Equals, IsAlphanumeric, IsBoolean, IsDateString, IsDefined, IsDivisibleBy, IsEmpty, IsEnum, IsIn, IsInt, IsNotEmpty, IsNumber, IsOptional, IsPositive, IsString, Max, Min } from 'class-validator';
+
+
+enum MovieGenre {
+    ACTION = 'action',
+    COMEDY = 'comedy',
+    DRAMA = 'drama',
+    HORROR = 'horror',
+    ROMANCE = 'romance',
+    SF = 'sf',
+    THRILLER = 'thriller',
+}
 
 /**
  * 영화 업데이트 DTO (Data Transfer Object)
@@ -16,4 +27,35 @@ export class UpdateMovieDto extends PartialType(CreateMovieDto) {
     @IsNotEmpty()
     @IsOptional() // title과 genre 둘 중 하나만 입력해도 됨
     genre?: string;
+
+    // @IsDefined().   : null || undefined 에러 던짐
+    // @IsOptional().  : null || undefined 통과
+    // @Equals('code test').  : 'code test'만 입력 가능
+    // @IsEmpty(). : null || undefined || '' 이면 통과
+    // @IsNotEmpty() : // null || undefined || '' 에러 던짐
+    // @IsIn(['action', 'comedy']) // action || comedy 만 입력 가능
+
+    // 타입 검증
+    // @IsBoolean()
+    // @IsString()
+    // @IsNumber()
+    // @IsInt()
+    // @IsEnum(MovieGenre) // 자동
+
+    // 날짜 검증
+    // @IsDateString()
+
+    // 숫자 검증
+    // @IsDivisibleBy(5) // 5로 나누어 떨어지는 수만 입력 가능
+    // @IsPositive() // 0보다 큰 수만 입력 가능
+    // @IsNegative() // 0보다 작은 수만 입력 가능
+    // @Min(100)
+    // @Max(1000)
+
+    // 문자 검증
+    // @Contains('code comedy')
+    @IsAlphanumeric() // 공백 포함 불가 (ex : code test -> 에러, codeTest -> 통과)
+    test: string;
+
+
 }
